685cb36d01dfc2643894358fb63b2f8cc5b5879c
rodineyw <rodineyw@yahoo.com.br>
2024-05-11 14:59:30 -0300

    add - new client

--- integrationWithSQL/main.py
+++ integrationWithSQL/main.py
@@ -6,17 +6,36 @@ from models import engine, Cliente, Conta
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Adicionar um novo cliente
-novo_cliente = Cliente(nome="Rod", cpf="123.123.123-12", email="rod@example.com")
-session.add(novo_cliente)
-session.commit()
+# Defirnir informações do cliente
+NOME = "Jordan"
+CPF = "56354986"
+EMAIL = "jordan@gmail.com"
 
-# Adicionando uma conta para o cliente
-nova_conta = Conta(cliente_id=novo_cliente.cliente_id, saldo=1000.0, tipo="corrente")
-session.add(nova_conta)
-session.commit()
+
+# Adicionar um novo cliente
+cliente = session.query(Cliente).filter_by(nome=NOME, cpf=CPF, email=EMAIL).first()
+if cliente:
+    print("Um cliente com estas informações {nome. cpf, email} já existe.")
+else:
+    novo_cliente = Cliente(nome=NOME, cpf=CPF, email=EMAIL)
+    session.add(novo_cliente)
+    try:
+        session.commit()
+        print("Cliente adicionado com sucesso.")
+
+        # Adicionando uma conta para o cliente recém-adicionado
+        nova_conta = Conta(
+            cliente_id=novo_cliente.cliente_id, saldo=1000.0, tipo="corrente"
+        )
+        session.add(nova_conta)
+        session.commit()
+
+    except ImportError as e:
+        session.rollback()
+        print(f"Erro ao adicionar cliente: {e}")
+
 
 # Consultando clientes
 clientes = session.query(Cliente).all()
 for cliente in clientes:
-    print(cliente.nome, cliente.cpf)
+    print(cliente.nome, cliente.cpf, cliente.email)
